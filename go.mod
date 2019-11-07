f6d2e20fe2104d74c2c5f0481295b7d22171dbb1
travisczerw <travis.czerw@gmail.com>
2019-11-07 14:06:12 -0600

    refactor(pods): Add support for ad pods

---
+++ go.mod
@@ -0,0 +1,8 @@
+module vendor
+
+go 1.13
+
+require (
+	github.com/favish/vmap v0.0.0-20190905152700-46c3c2b90acd // indirect
+	github.com/rs/vast v0.0.0-20180618195556-06597a11a4c3 // indirect
+)
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func getEnv(key, fallback string) string {
@@ -32,7 +33,7 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 	// Tell the browsers what to do with it
     //TODO: Consider reasonable cache tags, to reduce number of requests from clients
 
-	// Get the required descruption query param or tell them all is lost.
+	// Get the required description query param or tell them all is lost.
 	dkeys, dok := r.URL.Query()["description_url"]
 	if !dok || len(dkeys[0]) < 1 {
 		log.Println("")
@@ -42,15 +43,34 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 	}
 	descriptionUrl := dkeys[0]
 
-	// also require referrer to set CORS
+	// Require referrer to set CORS.
 	fkeys, _ := r.URL.Query()["referrer"]
 	if len(fkeys) > 0  {
-		//u, _ := url.Parse(fkeys[0])
 		w.Header().Set("Access-Control-Allow-Origin", fkeys[0])
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 	}
 
-	var test vmap.VMAP = vmap.VMAP{
+	// Require video duration in order to determine VMAP structure.
+	durkeys, dok := r.URL.Query()["duration"]
+	if !dok || len(durkeys[0]) < 1 {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("500 - Url Param 'duration' is missing"))
+		return
+	}
+    adGapSeconds := 15
+	duration, _ := strconv.Atoi(durkeys[0])
+	numberOfPods := duration/adGapSeconds
+	var adBreaks []vmap.AdBreak
+
+	fmt.Println(w, "Number of pods: %s", numberOfPods)
+
+	if numberOfPods > 0 {
+		for i := 1; i <= numberOfPods; i++ {
+			adBreaks = append(adBreaks, adBreakGenerator(i * adGapSeconds, descriptionUrl, "midroll", "15", "90", "3"))
+		}
+	}
+
+	var mainVmap vmap.VMAP = vmap.VMAP{
 		Version:    "1.0",
 		XmlNS:    "http://www.iab.net/videosuite/vmap",
 		AdBreaks:   []vmap.AdBreak{
@@ -103,7 +123,40 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 		},
 		Extensions: nil,
 	}
-	var xmlt, _ = xml.Marshal(test)
+	mainVmap.AdBreaks = append(mainVmap.AdBreaks, adBreaks...)
+	var xmlt, _ = xml.Marshal(mainVmap)
 	w.Header().Set("Content-type", "text/xml")
 	fmt.Fprintf(w, "%s", xmlt)
 }
+
+func adBreakGenerator(offset int, descriptionUrl string, breakId string, minSec string, maxSec string, maxPods string) vmap.AdBreak {
+	return vmap.AdBreak {
+		TimeOffset:     vmap.Offset{
+			Duration: nil,
+			Position: offset,
+			Percent:  0,
+		},
+		BreakType:      "linear",
+		BreakID:        breakId,
+		RepeatAfter:    0,
+		AdSource:       &vmap.AdSource{
+			ID:                "midroll-ad",
+			AllowMultipleAds: &t,
+			FollowRedirects:  &t,
+			VASTAdData:       nil,
+			AdTagURI:         &vmap.AdTagURI{
+				TemplateType: "vast3",
+				URI:          fmt.Sprintf("https://pubads.g.doubleclick.net/gampad/ads?iu=/21841313772/real_vision/midroll&env=vp&impl=s&correlator=&tfcd=0&npa=0&gdfp_req=1&output=vast&sz=640x480&unviewed_position_start=1&description_url=%s&pmnd=%s&pmxd=%s&pmad=%s",
+					descriptionUrl,
+					minSec,
+					maxSec,
+					maxPods,
+				),
+			},
+			CustomAdData:     nil,
+		},
+		TrackingEvents: nil,
+		Extensions:     nil,
+	}
+}
+
